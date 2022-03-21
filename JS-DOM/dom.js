58dab19a9bc5f4dcb810b3a66997b58fd223d244
Cayo Nakasato <cayonakasato112@gmail.com>
2022-03-21 16:53:49 -0400

    Segundo Commit

--- JS-DOM/dom.js
+++ JS-DOM/dom.js
@@ -1,205 +1,213 @@
-//array de obejtos(produtos)
+const listItem = document.getElementById("listNav");
+const btnBuscar = document.getElementById('btnFind');
+
 const produtos = [
-    {
-        img:".//Imagens/Men-Jacket-Front-Black__15466 1.svg",
-        tag: "Camisetas",
-        titulo:"Lightweight Jacket",
-        descricao:"Adicione um pouco de energia ao seu guarda-roupa de inverno com esta jaqueta vibrante...",
-        valor: 100.00,
-        id : 0
-    },
+  {
+    img: ".//Imagens/Men-Jacket-Front-Black__15466 1.svg",
+    tag: "Camisetas",
+    titulo: "Lightweight Jacket",
+    descricao:
+      "Adicione um pouco de energia ao seu guarda-roupa de inverno com esta jaqueta vibrante...",
+    valor: 100.0,
+  },
 
-    {
-        img:".//Imagens/image 1.svg",
-        tag: "Acessórios",
-        titulo:"Black Hat",
-        descricao:"O gorro Next.js chegou! Esta beldade bordada tem um ajuste confortável que garante que...",
-        valor: 100.00,
-        id : 1
-    },
+  {
+    img: ".//Imagens/image 1.svg",
+    tag: "Acessórios",
+    titulo: "Black Hat",
+    descricao:
+      "O gorro Next.js chegou! Esta beldade bordada tem um ajuste confortável que garante que...",
+    valor: 100.0,
+  },
 
-    {
-        img:".//Imagens/Surgical-Mask-Black__89554 1.svg",
-        tag: "Acessórios",
-        titulo:"Mask",
-        descricao:"Esta máscara facial durável é feita de duas camadas de tecido tratado e possui presilhas...",
-        valor: 40.00,
-        id : 2
-    },
+  {
+    img: ".//Imagens/Surgical-Mask-Black__89554 1.svg",
+    tag: "Acessórios",
+    titulo: "Mask",
+    descricao:
+      "Esta máscara facial durável é feita de duas camadas de tecido tratado e possui presilhas...",
+    valor: 40.0,
+  },
 
-    {
-        img:".//Imagens/Men-TShirt-Black-Front__70046 1.svg",
-        tag: "Camisetas",
-        titulo:"T-Shirt",
-        descricao:"Esta t-shirt é imprescindível no seu guarda-roupa, combinando o caimento intemporal de...",
-        valor: 100.00,
-        id : 3
-    },
+  {
+    img: ".//Imagens/Men-TShirt-Black-Front__70046 1.svg",
+    tag: "Camisetas",
+    titulo: "T-Shirt",
+    descricao:
+      "Esta t-shirt é imprescindível no seu guarda-roupa, combinando o caimento intemporal de...",
+    valor: 100.0,
+  },
 
-    {
-        img:".//Imagens/mockup-a0dc2330__62146 1.svg",
-        tag: "Camisetas",
-        titulo:"Short-Sleeve T-Shirt",
-        descricao:"Agora você encontrou a camiseta básica do seu guarda-roupa. É feito de um mais grosso...",
-        valor: 100.00,
-        id : 4
-    },
+  {
+    img: ".//Imagens/mockup-a0dc2330__62146 1.svg",
+    tag: "Camisetas",
+    titulo: "Short-Sleeve T-Shirt",
+    descricao:
+      "Agora você encontrou a camiseta básica do seu guarda-roupa. É feito de um mais grosso...",
+    valor: 100.0,
+  },
 
-    {
-        img:".//Imagens/mockup-9b9894f1__67347 1.svg",
-        tag: "Camisetas",
-        titulo:"Champion Packable Jacket",
-        descricao:"Proteja-se dos elementos com esta jaqueta embalável Champion. Esta jaqueta de poliést...",
-        valor: 100.00,
-        id : 5
-    }
-]
+  {
+    img: ".//Imagens/mockup-9b9894f1__67347 1.svg",
+    tag: "Camisetas",
+    titulo: "Champion Packable Jacket",
+    descricao:
+      "Proteja-se dos elementos com esta jaqueta embalável Champion. Esta jaqueta de poliést...",
+    valor: 100.0,
+  },
+];
 
-//Função que cria eleemntos da vitrine
-function createElementCard(arrayProdutos){
- const list = document.getElementById('products-list');
- list.innerHTML = "";
+let carrinhoDeCompras = [];
 
- for(let i = 0 ; i < arrayProdutos.length; i++){
+function createElementCard(arrayProdutos) {
+  const list = document.getElementById("products-list");
+  list.innerHTML = "";
 
-     const li = document.createElement('li');
-     const img = document.createElement('img');
-     const categoria = document.createElement('span');
-     const titulo = document.createElement('h2');
-     const descrição = document.createElement('p');
-     const preço = document.createElement('span');
-     const btnAdiciona = document.createElement('button');
+  for (let i = 0; i < arrayProdutos.length; i++) {
+    let produto = arrayProdutos[i];
 
-     li.classList.add('product');
-     categoria.classList.add('classe');
-     titulo.classList.add('productName');
-     preço.classList.add('preço');
-     btnAdiciona.classList.add('addCart');
+    const li = document.createElement("li");
+    const img = document.createElement("img");
+    const categoria = document.createElement("span");
+    const titulo = document.createElement("h2");
+    const descrição = document.createElement("p");
+    const preço = document.createElement("span");
+    const btnAdiciona = document.createElement("button");
 
-     img.src = arrayProdutos[i].img;
-     categoria.innerText = arrayProdutos[i].tag;
-     titulo.innerText = arrayProdutos[i].titulo;
-     descrição.innerText = arrayProdutos[i].descricao;
-     preço.innerText = 'R$' + arrayProdutos[i].valor;
-     btnAdiciona.innerText = "Adicionar ao carrinho";
+    li.classList.add("product");
+    categoria.classList.add("classe");
+    titulo.classList.add("productName");
+    preço.classList.add("preço");
+    btnAdiciona.classList.add("addCart");
 
-     li.appendChild(img);
-     li.appendChild(categoria);
-     li.appendChild(titulo);
-     li.appendChild(descrição);
-     li.appendChild(preço);
-     li.appendChild(btnAdiciona);
+    img.src = produto.img;
+    categoria.innerText = produto.tag;
+    titulo.innerText = produto.titulo;
+    descrição.innerText = produto.descricao;
+    preço.innerText = "R$" + produto.valor;
+    btnAdiciona.innerText = "Adicionar ao carrinho";
 
-     list.appendChild(li);
- }
-}
-//chamando a função para criar os elementos
-createElementCard(produtos);
+    btnAdiciona.addEventListener("click", function (e) {
+      carrinhoDeCompras.push(produto);
 
-const carrinhoDeCompras = [];
-//pegando um array com todos os botoes com classe
-const botaodeadd = document.querySelectorAll('.addCart');
+      itenInCart(carrinhoDeCompras);
 
-    const quantidadeItens = document.getElementsByClassName('quantidadeItens')
+      let total = 0;
+      let count = carrinhoDeCompras.length;
 
-//criando o itens dentro do carrinho quando clicados
-for(let i = 0; i < botaodeadd.length; i++){
+        for (let j = 0; j < carrinhoDeCompras.length; j++){
+          
+          const quantidadeItens = document.getElementById('quantidadeItens');
+          quantidadeItens.innerText = `${count}`;
 
-//selecionando qual botao foi selecionado
-botaodeadd[i].addEventListener("click",(event) =>{
-    const li = event.target.closest('li');
-    let url = li.children[0].src;
-    let titulo = li.children[2].innerText;
-    let valor = li.children[4].innerText;
+          const valorAtual = produto.valor;
 
-        carrinhoDeCompras.push(1);
-        quantidadeItens.innerText = carrinhoDeCompras.length;
-        console.log(carrinhoDeCompras.length)
+          total += valorAtual;
 
-    itenInCart(url, titulo, valor);
-})
-
-//criando itens para serem adicionados no carrinho
-function itenInCart(url, titulo, valor){
-
-    const shopList = document.getElementById('cartproducts');
+          const valorTotal = document.getElementById('valorTotal');
+          valorTotal.innerText = `R$${total}.00`;
     
-    const li = document.createElement('li');
-    const img = document.createElement('img');
-    const div = document.createElement('div')
-    const title = document.createElement('h2');
-    const price = document.createElement('span');
-    const btnRemove = document.createElement('button');
+          console.log(total);
+          console.log(carrinhoDeCompras)
+        }
+    });
 
-    li.classList.add('produtsincart');
-    div.classList.add('cartinfo');
-    price.classList.add('preçocarrinho');
-    btnRemove.classList.add('btnremove');
+    li.appendChild(img);
+    li.appendChild(categoria);
+    li.appendChild(titulo);
+    li.appendChild(descrição);
+    li.appendChild(preço);
+    li.appendChild(btnAdiciona);
 
-    img.src = url;
-    title.innerText = titulo;
-    price.innerText = valor;
-    btnRemove.innerText = "Remover Produto"
-               
+    list.appendChild(li);
+  }
+}
+
+function itenInCart(produto) {
+  const shopList = document.getElementById("cartproducts");
+  shopList.innerHTML = ''
+  for (let i = 0; i < produto.length; i++) {
+    const item = produto[i];
+
+    const li = document.createElement("li");
+    const img = document.createElement("img");
+    const div = document.createElement("div");
+    const title = document.createElement("h2");
+    const price = document.createElement("span");
+    const btnRemove = document.createElement("button");
+
+    li.classList.add("produtsincart");
+    div.classList.add("cartinfo");
+    price.classList.add("preçocarrinho");
+    btnRemove.classList.add("btnremove");
+
+    btnRemove.addEventListener("click", function (e) {
+      const textButton = btnRemove.parentElement.children[1].textContent;
+
+      const indexItem = carrinhoDeCompras.indexOf(textButton);
+
+      carrinhoDeCompras.splice(indexItem,1);
+
+      btnRemove.parentElement.parentElement.remove();
+      
+      const quantidadeItens = document.getElementById('quantidadeItens');
+      quantidadeItens.innerText = `${count - 1}`;
+
+      const valorTotal = document.getElementById('valorTotal');
+      valorTotal.innerText = `R$${total - 100}.00`;
+
+      console.log(carrinhoDeCompras)
+    });
+
+    img.src = item.img;
+    title.innerText = item.titulo;
+    price.innerText = `R$ ${item.valor}.00`;
+    btnRemove.innerText = "Remover Produto";
+
     div.appendChild(title);
     div.appendChild(price);
     div.appendChild(btnRemove);
 
-    li.appendChild(img)
+    li.appendChild(img);
     li.appendChild(div);
-                    
+
     shopList.appendChild(li);
+  }
+}
+
+function filterPost(e) {
+  const newData = [];
+  const item = e.target;
+  const filter = item.dataset.tag;
+
+  if (filter === "Todos") {
+    createElementCard(produtos);
+  } else {
+    for (let i = 0; i < produtos.length; i++) {
+      if (produtos[i].tag.indexOf(filter) !== -1) {
+        newData.push(produtos[i]);
+      }
     }
+    createElementCard(newData);
+  }
 }
 
-//pegando a ul da navbar
-const listItem = document.getElementById('listNav');
+function findProduct(){
+  const inputValue = document.getElementById('btnBuscar');
+  const newData = [];
 
-//função que filtra os itens pela navBar
-function filterPost(e){
-    const newData = [];
-    const item = e.target;
-    const filter = item.dataset.tag;
-    
-    if(filter === "Todos"){
-        createElementCard(produtos);
-    }else{
-        for(let i = 0; i < produtos.length; i++){
-            if(produtos[i].tag.indexOf(filter) !== -1){
-                newData.push(produtos[i]);
-            }
-        }
-        createElementCard(newData);
+  console.log(inputValue.value)
+  for (let i = 0; i < produtos.length; i++) {
+    if(produtos[i].titulo.indexOf(inputValue.value) !== -1){
+      newData.push(produtos[i]);
     }
-   
-}
-
-//evento que realiza os filtros quando clicado
-listItem.addEventListener('click', filterPost);
-
-//função que informa qual botao dentro do carrinho foi clicado
-function clickedIten(e){
-    const itemClick = e.target;
-    if(itemClick.tagName === "BUTTON"){
-        removeItem(itemClick);
-    }
-}
-
-//apagando o produto que foi clicado
-cartproducts.addEventListener('click', clickedIten);
-
-
-//função para remover o item do carrinho
-function removeItem(btnremove){
-    const li = btnremove.parentElement.parentElement;
-    const indexItem = carrinhoDeCompras.indexOf(li);
-
-    carrinhoDeCompras.splice(indexItem, 1)
-
-    btnremove.parentElement.parentElement.remove();
-    console.log(carrinhoDeCompras)
+  }
+  createElementCard(newData);
 }
 
 
+btnBuscar.addEventListener('click', findProduct);
 
+listItem.addEventListener("click", filterPost);
 
+createElementCard(produtos);
